5d3252f3acfbfc357d7246528c4a010b0b05ddac
EnzoManuel12B EnzoManuel12B <enzomaneco@gmail.com>
2024-04-20 00:56:40 +0000

    javascript do agradecimento

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let botoes = document.querySelectorALL(".botoes button")
+let botoes = document.querySelectorAll(".botoes button")
 botoes.forEach(botao => {
     botao.addEventListener("click", guardaNota)
 })
